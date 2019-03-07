0a062d48daa828b79e0e4e96aa7669c8294e8994
Abou-Shamalah, Sedky <Sedky.Abou-Shamalah@londonlife.com>
2019-03-07 10:41:03 -0500

    small cleanup

--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
-Java spring-boot starter
+# Contains
+Java 
+spring-boot
 Maven
 JPA
+
+
+# DB
+Has in-memory H2 DB, accessed via:
+http://localhost:8080/h2-console/login.jsp?
--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class MyController {
 
